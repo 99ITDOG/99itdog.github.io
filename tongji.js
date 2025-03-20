5c553be0be08b7475159332ba25d03196a05890f
99ITDOG <99itdog@gmail.com>
2025-03-20 15:26:59 +0800

    Delete tongji.js

--- tongji.js
+++
@@ -1,8 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?9257d847d44063644e5b11111e6973fa";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
-//统计代码
